825615ed807886cde3c39ecb115ea768fb8badec
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-31 19:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function updateWindowSize() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let sizeInfoDiv = document.getElementById("sizeInfo");
+    sizeInfoDiv.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+}
+window.addEventListener("resize", updateWindowSize);
+updateWindowSize();
